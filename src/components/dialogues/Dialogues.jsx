cc5e645e00680c60fc79f635ce7b2e4251d1b407
Liz <lizziekuznetsova@gmail.com>
2025-10-23 20:32:13 +0100

    made the messages code more universal and flexible

--- src/components/dialogues/Dialogues.jsx
+++ src/components/dialogues/Dialogues.jsx
@@ -2,22 +2,79 @@ import './Dialogue.css'
 import {DialogueName, Test} from './dialogueName/DialogueName'
 import Messages from './messages/Messages'
 
+let dialogueNames=[
+    {
+        name: "Anya Taylor-Joy",
+        id: "1",
+        like: 10
+    },
+    {
+        name: "Florence Pugh",
+        id: "2",
+        like: 15
+    },
+    {
+        name: "Olivia Cooke",
+        id: "3",
+        like: 22
+    },
+    {
+         name: "Andrew Garfield",
+        id: "4",
+        like: 9
+    },
+    {
+         name: "Timmy Jones",
+        id: "5",
+        like: 9
+    },
+    {
+         name: "Katie Vermont",
+        id: "6",
+        like: 9
+    }
+    
+]
+
+let dialogueMessages=[
+    {
+        message: "Hi, how are you?",
+        id: "0"
+    },
+     {
+        message: " Are you free tomorrow?",
+        id: "1"
+    },
+       {
+        message: "Btw I borrowed your white sweater",
+        id: "2"
+    },
+       {
+        message: "yooooooo",
+        id: "3"
+    }
+]
+// dialogueNames[2].name="lucy"
+console.log(dialogueNames[2].name)
+
 
 export default function Dialogues(){
     return(
         <div className="dialogues profile">
             <div className="names">
-                <DialogueName id={"1"} name={"Anya Taylor-Joy"}/>
-                <DialogueName id={"2"} name={"Florence Pugh"}/>
-                <DialogueName id={"3"} name={"Olivia Cooke"}/>
-                <DialogueName id={"4"} name={"Andrew Garfield"}/>
+                {dialogueNames.map((item) => <DialogueName id={item.id} name={item.name}/>) }
+                {/* <DialogueName id={dialogueNames[0].id} name={dialogueNames[0].name}/>
+                <DialogueName id={dialogueNames[1].id} name={dialogueNames[1].name}/>
+                <DialogueName id={dialogueNames[2].id} name={dialogueNames[2].name}/>
+                <DialogueName id={dialogueNames[3].id} name={dialogueNames[3].name}/> */}
             </div>
                 <div>
-                <Messages messages={"Hi, how are you?"}/>
-                <Messages messages={" Are you free tomorrow?"}/>
-                <Messages messages={"Btw I borrowed your white sweater"}/>
-                <Messages messages={"yooooooo"}/>
-                <Messages messages={"Btw I borrowed your white sweater"}/>
+                    {dialogueMessages.map((element) => <Messages messages={element.message}/>)}
+                {/* <Messages messages={dialogueMessages[0].message}/>
+                <Messages messages={dialogueMessages[1].message}/>
+                <Messages messages={dialogueMessages[2].message}/>
+                <Messages messages={dialogueMessages[3].message}/> */}
+                {/* <Messages messages={"Btw I borrowed your white sweater"}/> */}
                 </div>
 
              <div className="send_message">
